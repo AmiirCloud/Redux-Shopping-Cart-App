bf86c7d0b0fe724b459c01df089a9d79618b9d00
AmirMuse <amirlearn08@gmail.com>
2023-07-04 18:07:36 -0700

    Finished The login Functionality

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,33 @@
 import React from "react";
-
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/Auth";
 import "./Auth.css";
 
+
 const Auth = () => {
+  const userRef  = useRef()
+  const passRef  = useRef()
+  const login  = useDispatch()
+  const handleSubmit=(e=>{
+    e.preventDefault()
+    const userName =  userRef.current.value
+    const password =  passRef.current.value
+    
+    const values = {userName,password}
+    login(
+      authActions.login(values),
+      
+  )
+  })
   return (
     <div className="container">
       <h1>Login</h1>{" "}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="id">Id</label>
-        <input type="text" name="id" id="id" />
+        <input type="text" name="id" ref={userRef} id="id"  />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" ref={passRef} id="password" />
         <button className="login-btn" type="submit">
           Login
         </button>
--- src/store/Auth.js
+++ src/store/Auth.js
@@ -2,12 +2,20 @@ import {createSlice} from '@reduxjs/toolkit'
 const initialState = {
     isloged:false
 }
+const  userName = "amir"
+const password  ="123"
 const Auth  = createSlice({
     name:"auth",
  initialState:initialState,
  reducers:{
-    login(state){
-    state.isloged = true
+    login(state,actions){
+        console.log(actions.payload.userName)
+        if(actions.payload.userName === userName  && actions.payload.password === password){
+            state.isloged = true
+        }else{
+            console.log("invalid UserName or Password")
+            state.isloged =  false
+        }
     },
     logOut(state){
         state.isloged = false
