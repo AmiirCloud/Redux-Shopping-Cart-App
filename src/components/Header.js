e892be5e3b15bb25c6979fcc1ccaa401ed0a1044
AmirMuse <amirlearn08@gmail.com>
2023-07-04 18:37:04 -0700

    added Logout button, installed Link in react-router-dom  and So some Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Cart from "./Cart";
 import "./Header.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
       <nav className="header-nav">
-        <ul className="header-ul">
-          <li>
             <h2
               className="header-h2"
               style={{ fontFamily: "monospace", fontSize: "30px" }}
             >
               Redux Shopping App
             </h2>
+        <ul className="header-ul">
+        
+          <li>
+
+          <Link to="/logout" className="logout">
+            logout
+          </Link>
           </li>
+            
           <li>
             <Cart />
           </li>
